43fe1bd013a3db9292d90f1f16c0044e7f378bfc
Rviyer2000 <vaidyanathaiyerr@gmail.com>
2024-08-14 11:15:39 +0400

    added led blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 
-
-
 void setup() {
-  pinMode (12,INPUT_PULLUP);
+  pinMode (12,OUTPUT);
 
 }
 
 void loop() {
+  digitalWrite(12,HIGH);
+  delay(1000);
+  digitalWrite(12,HIGH);
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
 
